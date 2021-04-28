0405340b2e9da1e22058f2ef9dcfb2bb6366e89d
Bilel AIT ALLA <81183472+BilelAITALLA@users.noreply.github.com>
2021-04-28 14:49:09 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/JSON/json_helper.dart';
+import 'package:netflix/SQL/database_helper.dart';
+import 'package:netflix/models/movie_model.dart';
 import 'package:netflix/screens/home_screen.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
+void main() async {
+  DatabaseHelper helper = DatabaseHelper();
   WidgetsFlutterBinding.ensureInitialized();
+  List<Movie> movie = await fetchMovies(http.Client());
+  List<Movie> carousel = await fetchCarousel(http.Client());
+  List<Movie> tablemovies = await helper.getMovieList();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]).then((_) {
-    runApp(MyApp());
+    runApp(MyApp(
+      movies: movie,
+      carousel: carousel,
+      list: tablemovies,
+    ));
   });
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  final List<Movie> movies;
+  final List<Movie> carousel;
+  final List<Movie> list;
+  MyApp({this.movies, this.carousel, this.list});
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Netflix UI Redesign',
+      title: 'ANIMEX',
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomeScreen(
+        movielist: widget.movies,
+        carousel: widget.carousel,
+        list: widget.list,
+      ),
     );
   }
 }
